c48bbbdc3bced4f3db97056f29574c31dc1ff0af
mazax1ct <vova@giveandget.ru>
2019-10-16 20:23:05 +0300

    finish

--- build/js/main.js
+++ build/js/main.js
@@ -5,7 +5,15 @@ var resize_scroll = function(e) {
   $(window).scrollTop() > offset.height()
     ? header.addClass("fixed")
     : header.removeClass("fixed");
+
+
+  $(window).scrollTop() > offset.height() - header.height()*2
+    ? header.addClass("fixed2")
+    : header.removeClass("fixed2");
+
 };
+
+var apiFullPage;
 
 $(document).ready(function () {
   //запуск функции навешивания класса с тенью на шапку
@@ -13,47 +21,71 @@ $(document).ready(function () {
 
   //главный баннер
   if ($('.js-slider').length) {
-    $('.js-slider').slick({
-      auto: false,
-      mobileFirst: true,
-      slidesToShow: 1,
-      infinite: true,
-      arrows: false,
-      dots: true,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            focusOnSelect: true,
-            edgeFriction: 0,
-            centerMode: false,
-            variableWidth: false,
-            slidesToShow: 4,
-            arrows: true,
-            prevArrow: '<button type="button" class="slick-prev slick-arrow" title="Назад"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-left"/></svg></button>',
-            nextArrow: '<button type="button" class="slick-next slick-arrow" title="Вперед"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-right"/></svg></button>',
-            appendArrows: $('.team-slider-arrows'),
-            appendDots: $('.team-slider-dots')
+    if($('body').width() < 1199) {
+      $('.js-slider').slick({
+        auto: false,
+        mobileFirst: true,
+        slidesToShow: 1,
+        infinite: true,
+        arrows: false,
+        dots: true
+      });
+    } else {
+      var indexForNormalScroll = 2;
+
+      var apiFullPage = new fullpage('#fullpage', {
+        menu: "#slider_menu",
+        lockAnchors: true,
+        scrollBar: true,
+        fitToSection: false,
+
+
+        afterLoad: function(origin, destination, direction) {
+          if (destination.index === indexForNormalScroll) {
+            $('html').toggleClass('fp-scroll-free');
+            fullpage_api.setAutoScrolling(false);
           }
         },
-        {
-          breakpoint: 767,
-          settings: {
-            focusOnSelect: true,
-            edgeFriction: 0,
-            centerMode: false,
-            variableWidth: false,
-            slidesToShow: 3,
-            arrows: true,
-            prevArrow: '<button type="button" class="slick-prev slick-arrow" title="Назад"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-left"/></svg></button>',
-            nextArrow: '<button type="button" class="slick-next slick-arrow" title="Вперед"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-right"/></svg></button>',
-            appendArrows: $('.team-slider-arrows'),
-            appendDots: $('.team-slider-dots')
-          }
+      });
+
+      window.addEventListener('scroll', scrollHandler);
+
+      function scrollHandler() {
+        var normalSection = document.querySelectorAll(".section")[indexForNormalScroll];
+        var offset = normalSection.offsetTop;
+
+        if (getScrollTop() < offset && !fullpage_api.getFullpageData().autoScrolling) {
+          $('html').toggleClass('fp-scroll-free');
+          fullpage_api.setAutoScrolling(true);
+        } else if (getScrollTop() > offset && fullpage_api.getFullpageData().autoScrolling) {
+          $('html').toggleClass('fp-scroll-free');
+          fullpage_api.setAutoScrolling(false);
         }
-      ]
-    });
+      }
+
+      function getScrollTop() {
+        var doc = document.documentElement;
+        return (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+      }
+    }
   }
+
+  //переключение слайда в главном баннере
+  $('.js-slide').click(function () {
+    var href = $(this).attr('href');
+    console.log(href);
+    apiFullPage.moveTo(href);
+  });
+
+  //переключение слайда в главном баннере вверх
+  $('.js-slide-up').click(function () {
+    apiFullPage.moveSectionUp();
+  });
+
+  //переключение слайда в главном баннере вниз
+  $('.js-slide-down').click(function () {
+    apiFullPage.moveSectionDown();
+  });
 
   //слайдер "команда"
   if ($('.js-team-slider').length) {
@@ -113,7 +145,29 @@ $(document).ready(function () {
 
   //слайдер картинок в кейсах
   if ($('.js-case-slider').length) {
-    $('.js-case-slider').slick({
+    $('.js-case-slider').each(function (index) {
+      $('.js-case-slider').eq(index).slick({
+        auto: false,
+        mobileFirst: true,
+        slidesToShow: 1,
+        infinite: true,
+        arrows: false,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              arrows: true,
+              prevArrow: '<button type="button" class="slick-prev slick-arrow" title="Назад"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-left"/></svg></button>',
+              nextArrow: '<button type="button" class="slick-next slick-arrow" title="Вперед"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-right"/></svg></button>',
+              appendArrows: $(this).next('.case-slider-nav').find('.case-slider-arrows'),
+              appendDots: $(this).next('.case-slider-nav').find('.case-slider-dots')
+            }
+          }
+        ]
+      });
+    });
+    /*$('.js-case-slider').slick({
       auto: false,
       mobileFirst: true,
       slidesToShow: 1,
@@ -127,12 +181,12 @@ $(document).ready(function () {
             arrows: true,
             prevArrow: '<button type="button" class="slick-prev slick-arrow" title="Назад"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-left"/></svg></button>',
             nextArrow: '<button type="button" class="slick-next slick-arrow" title="Вперед"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#slider-right"/></svg></button>',
-            appendArrows: $('.case-slider-arrows'),
-            appendDots: $('.case-slider-dots')
+            appendArrows: $(this).next('.case-slider-nav').find('.case-slider-arrows'),
+            appendDots: $(this).next('.case-slider-nav').find('.case-slider-dots')
           }
         }
       ]
-    });
+    });*/
   }
 
   //проверка на пустоту поля ввода при загрузке страницы
@@ -153,6 +207,9 @@ $(document).ready(function () {
 
   //открваем мобильное меню
   $('.js-menu-open').click(function () {
+    if($('body').width() > 1199) {
+      apiFullPage.setAllowScrolling(false);
+    }
     $('body').toggleClass('overflow');
     $('.menu').fadeIn('300', function () {
       $('.menu__inner').animate({
@@ -170,6 +227,9 @@ $(document).ready(function () {
       }, 400, function() {
         $('.menu').fadeOut('300', function () {
           $('body').toggleClass('overflow');
+          if($('body').width() > 1199) {
+            apiFullPage.setAllowScrolling(true);
+          }
         });
     });
     return false;
@@ -185,6 +245,9 @@ $(document).ready(function () {
         left: "-100%"
       }, 400, function() {
         $('.menu').fadeOut('300', function () {
+          if($('body').width() > 1199) {
+            apiFullPage.setAllowScrolling(true);
+          }
           $('body').toggleClass('overflow');
           $('body,html').animate({
             scrollTop: top
